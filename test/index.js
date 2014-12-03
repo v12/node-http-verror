19590edb453348cf29a469f370cedb697464bb69
Nikita Khomyakov <dr.evil@krtn.eu>
2014-12-03 14:53:50 +0000

    Switched to Node built-in assert module instead of expect.js for testing

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var expect = require('expect.js'),
+/* jshint mocha:true */
+
+var assert = require('assert'),
     errors = require('../lib');
 
 describe('General', function () {
@@ -11,15 +13,15 @@ describe('General', function () {
     it('should return wrapped error with status 400 (BadRequest) and default message', function () {
         var e = new errors.BadRequest();
 
-        expect(e.statusCode).to.be.equal(400);
-        expect(e.message).to.be.equal('Invalid data was sent to the server');
+        assert.strictEqual(e.statusCode, 400);
+        assert.strictEqual(e.message, 'Invalid data was sent to the server');
     });
 
     it('should return Forbidden (403) error with preceding error', function () {
         var e = new errors.Forbidden(new Error('test error'), 'we don\'t show that it was test error');
 
-        expect(e.statusCode).to.be.equal(403);
-        expect(e.message).to.be.equal('we don\'t show that it was test error');
-        expect(e.cause().message).to.be.equal('test error');
+        assert.strictEqual(e.statusCode, 403);
+        assert.strictEqual(e.message, 'we don\'t show that it was test error');
+        assert.strictEqual(e.cause().message, 'test error');
     });
 });

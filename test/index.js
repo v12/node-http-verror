dfa3df03b0eb29f277a833d8f14079bcb0964750
Nikita Khomyakov <dr.evil@krtn.eu>
2015-05-01 18:03:52 +0100

    Use Chai instead of native assert

--- test/index.js
+++ test/index.js
@@ -2,26 +2,34 @@
 
 /* jshint mocha:true */
 
-var assert = require('assert'),
-    errors = require('../lib');
+var errors = require('../lib'),
+    expect = require('chai').expect;
 
-describe('General', function () {
+describe('http-verror', function () {
     it('should return an object with error functions', function () {
-
+        expect(errors).to.be.an('object');
     });
 
     it('should return wrapped error with status 400 (BadRequest) and default message', function () {
         var e = new errors.BadRequest();
 
-        assert.strictEqual(e.statusCode, 400);
-        assert.strictEqual(e.message, 'Invalid data was sent to the server');
+        expect(e).to.have.all.keys('statusCode', 'message');
+
+        expect(e.statusCode).to.equal(400);
+        expect(e.message).to.equal('Invalid data was sent to the server');
     });
 
     it('should return Forbidden (403) error with preceding error', function () {
         var e = new errors.Forbidden(new Error('test error'), 'we don\'t show that it was test error');
 
-        assert.strictEqual(e.statusCode, 403);
-        assert.strictEqual(e.message, 'we don\'t show that it was test error');
-        assert.strictEqual(e.cause().message, 'test error');
+        expect(e.statusCode).to.equal(403);
+        expect(e.message).to.equal('we don\'t show that it was test error');
+
+        expect(e.cause().message).to.equal('test error');
+    });
+
+    it('should be an instance of HttpError, which is publicly exposed', function () {
+        expect(new errors.BadRequest()).to.be.an.instanceof(errors.HttpError);
+        expect(new errors.NotFound()).to.be.an.instanceof(errors.HttpError);
     });
 });
